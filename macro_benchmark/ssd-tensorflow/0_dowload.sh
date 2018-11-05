0e4500e648639ded50e3b5eb26ede68f76d19d3d
wz.ww <wz.ww@alibaba-inc.com>
2018-11-05 14:10:42 -0800

    Remove old version

--- macro_benchmark/ssd-tensorflow/0_dowload.sh
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/balancap/SSD-Tensorflow.git
-unzip SSD-Tensorflow/checkpoints/ssd_300_vgg.ckpt.zip -d  SSD-Tensorflow/checkpoints/
--- macro_benchmark/ssd-tensorflow/1_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir results
-python ssd_notebook.py | tee ./results/result.txt
