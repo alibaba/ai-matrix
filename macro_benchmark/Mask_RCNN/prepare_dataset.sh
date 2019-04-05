c3099bc4211212a85943f8fb8338cfb970493107
Zhaoxuepeng <495149700@qq.com>
2019-04-05 13:19:16 +0800

    Update prepare_dataset.sh
    Fix error for :"./prepare_dataset.sh: line 7: [: missing `]'"

--- macro_benchmark/Mask_RCNN/prepare_dataset.sh
+++ macro_benchmark/Mask_RCNN/prepare_dataset.sh
@@ -4,7 +4,7 @@ if [ ! -f "mask_rcnn_coco.h5" ]; then
     wget https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5
 fi
 
-if [ ! -f "coco_dataset/train2014.zip" ] || [ ! -f "coco_dataset/val2014.zip" ] || [ ! -f "coco_dataset/annotations_trainval2014.zip"]; then
+if [ ! -f "coco_dataset/train2014.zip" ] || [ ! -f "coco_dataset/val2014.zip" ] || [ ! -f "coco_dataset/annotations_trainval2014.zip" ]; then
 	python download_coco.py
 fi
 
