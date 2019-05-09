0cb9e7cb3e86699fb441355e8dbeb0fa34f21e29
wz.ww <wz.ww@alibaba-inc.com>
2019-05-08 18:17:23 -0700

    Update setup script for tensorrt ssd

--- macro_benchmark/SSD_Caffe/tensorrt/sampleSSD/setup.sh
+++ macro_benchmark/SSD_Caffe/tensorrt/sampleSSD/setup.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-cd ../data
+cd ..
+
+if [ ! -d data ]; then
+    mkdir data
+fi
+
+cd data
+
 git clone https://github.com/aimatrix-alibaba/ssd-caffe-trt.git
 mv ssd-caffe-trt/ssd.tar.gz ./
 tar -xzvf ssd.tar.gz
 cd ../sampleSSD
+
+echo "Running md5 checksum on downloaded dataset ..."
+if md5sum -c checksum.md5; then
+        echo "Dataset checksum pass"
+else
+        echo "Dataset checksum fail"
+        exit 1
+fi
