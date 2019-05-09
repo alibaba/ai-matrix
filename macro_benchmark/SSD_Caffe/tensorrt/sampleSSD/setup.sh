29d83c0a057328345c42ec39e197860cab7c77cc
Frank Wei <w.wei@alibaba-inc.com>
2019-05-09 11:26:04 -0700

    fix checksum issue

--- macro_benchmark/SSD_Caffe/tensorrt/sampleSSD/setup.sh
+++ macro_benchmark/SSD_Caffe/tensorrt/sampleSSD/setup.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
+make
+cd ../data
 
-cd ..
-
-if [ ! -d data ]; then
-    mkdir data
-fi
-
-cd data
-
-git clone https://github.com/aimatrix-alibaba/ssd-caffe-trt.git
-mv ssd-caffe-trt/ssd.tar.gz ./
-tar -xzvf ssd.tar.gz
-cd ../sampleSSD
-
-echo "Running md5 checksum on downloaded dataset ..."
-if md5sum -c checksum.md5; then
+if [ ! -d ssd ]; then
+  git clone https://github.com/aimatrix-alibaba/ssd-caffe-trt.git
+  mv ssd-caffe-trt/ssd.tar.gz ./
+  tar -xzvf ssd.tar.gz
+  echo "Running md5 checksum on downloaded dataset ..."
+  if md5sum -c checksum.md5; then
         echo "Dataset checksum pass"
-else
+  else
         echo "Dataset checksum fail"
         exit 1
+  fi
 fi
+
+cd ../sampleSSD
+
