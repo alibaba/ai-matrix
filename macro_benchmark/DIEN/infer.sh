12a3351cfb81ffce3b8677044ea24a09c25932b1
wz.ww <wz.ww@alibaba-inc.com>
2019-05-20 18:24:15 -0700

    Change unit from time to recommendations/s for NCF and DIEN

--- macro_benchmark/DIEN/infer.sh
+++ macro_benchmark/DIEN/infer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TOTAL_RECOMMDS=606720
+
 if [ -d results ]; then
     mv results results_$(date +%Y%m%d%H%M%S)
 fi
@@ -15,8 +17,13 @@ do
 	start=`date +%s%N`
 	python script/train.py --mode=test --batch_size=$batch |& tee results/result_infer_${batch}.txt
 	end=`date +%s%N`
-	total_time=`bc <<< "scale = 3; ($end-$start)/1000000000"`
-	echo "System performance in seconds is: $total_time" >> results/result_infer_${batch}.txt
+	total_time=$(((end-start)/1000000))
+    #total_time=`bc <<< "scale = 3; ($end-$start)/1000000000"`
+    total_images=$TOTAL_RECOMMDS
+    system_performance=$((1000*$total_images/$total_time))
+    echo "Total recommendations: $total_images" >> results/result_infer_${batch}.txt
+    echo "System time in miliseconds is: $total_time" >> results/result_infer_${batch}.txt
+    echo "System performance in recommendations/second is: $system_performance" >> results/result_infer_${batch}.txt
 done
 
 python process_results.py --infer
