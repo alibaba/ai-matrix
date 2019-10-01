a86a34c940e4bb80de5d9f15921f93d0adc628b4
wz.ww <wz.ww@alibaba-inc.com>
2019-09-30 18:26:12 -0700

    Remove large lfs files NCF

--- macro_benchmark/NCF/setup.sh
+++ macro_benchmark/NCF/setup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 pip3 install numpy==1.16.1
+
+if [ ! -f "Data/train_data.npy" ]; then
+        cd Data
+    wget https://zenodo.org/api/files/950b6f8e-9676-462e-9704-6e065dde09f7/train_data.npy
+    cd ..
+fi
+
+echo "Running md5 checksum on downloaded dataset ..."
+if md5sum -c checksum.md5; then
+        echo "Dataset checksum pass"
+else
+        echo "Dataset checksum fail"
+        exit 1
+fi
