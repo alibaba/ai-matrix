3b0cf0c520eae71c76dd65a74a6132c055702ab9
rabbitwayne <wz6pc@virginia.edu>
2020-02-07 15:40:21 -0800

    Remove older version of BERT Pytorch

--- macro_benchmark/BERT_PyTorch/scripts/docker/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build . --rm -t bert_pyt
--- macro_benchmark/BERT_PyTorch/scripts/docker/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm \
-  --runtime=nvidia \
-  --shm-size=1g \
-  --ulimit memlock=-1 \
-  --ulimit stack=67108864 \
-  -v ${PWD}:/workspace/bert \
-  bert_pyt bash
