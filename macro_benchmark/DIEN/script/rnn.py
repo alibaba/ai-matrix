de07104a5253303b0fda8d1b8cfb2d39aeb439c2
Frank Wei <w.wei@alibaba-inc.com>
2020-09-02 21:26:15 -0700

    Update rnn.py

--- macro_benchmark/DIEN/script/rnn.py
+++ macro_benchmark/DIEN/script/rnn.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Modifications copyright (C) 2018 Alibaba Group
 # ==============================================================================
 
 """RNN helpers for TensorFlow models.
